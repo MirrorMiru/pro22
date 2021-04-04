04f730e41b8c5c652812c07ca97f6ae3654e46fa
MirrorMiru <80078855+MirrorMiru@users.noreply.github.com>
2021-04-03 18:00:58 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,12 +10,12 @@ const Body = Matter.Body;
 
 function preload()
 {
-	starImg = loadImage("images/star.png");
-	bgImg = loadImage("images/starNight.png");
+	starImg = loadImage("star.png");
+	bgImg = loadImage("starNight.png");
 	//load animation for fairy here
-	fairyImg = loadAnimation("images/fairyImage1.png","images/fairyImage2.png")
+	fairyImg = loadAnimation("fairyImage1.png","fairyImage2.png")
 
-	music = loadSound("sound/JoyMusic.mp3")
+	music = loadSound("JoyMusic.mp3")
 }
 
 function setup() {
